96d415b4fb29dbc7cb134b48042c9ac6a07d156b
haroun3amri <haroun3amri@gmail.com>
2019-05-30 14:29:10 +0200

    Ajoutez des fichiers projet.

---
+++ GRATHERMAN/GRATHERMAN.MobileAppService/Models/ICollectedInformationRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GRATHERMAN.MobileAppService.Models
+{
+    public interface ICollectedInformationRepository
+    {
+        void Add(CollectedInformation c);
+        void Update(CollectedInformation item);
+        CollectedInformation Remove(string key);
+        CollectedInformation Get(string id);
+        IEnumerable<CollectedInformation> GetAll();
+    }
+}
---
+++ GRATHERMAN/GRATHERMAN/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GRATHERMAN.Models;
+
+namespace GRATHERMAN.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
